0188c21330e5ed79115eb482a33b4bd1c1defeb5
cosmas-obinna <obiynwajesus@gmail.com>
2021-08-08 21:11:08 +0100

    second commit

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-hello world!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+</head>
+<body>
+    <h1> Hello World </h1><\br>
+    <h3> This is y first php training web development.</h3><\br>
+    <h3> I really hope to enjoy. I was motivated to take this course</h3><\br>
+    <h3> Because of the series of courses I have taken on sql</h3><\br>
+
+<?php
+    $name = 'Cosmas Nworie';
+    $Designation = 'Business Data Analyst';
+    echo "My name is: $name and I work as $Designation"
+?>
+
+
+
+
+
+</body>
+</html>
