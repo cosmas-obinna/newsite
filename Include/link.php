3e796f3d4e5690be06f6780245d7848458b74d27
cosmas-obinna <obiynwajesus@gmail.com>
2021-08-25 16:27:35 +0100

    modification

--- Include/link.php
+++ Include/link.php
@@ -1,22 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Start</title>
 </head>
 <body>
+    
+    <h2>follow each link to the respective topics</h2>
     <ul>
-            <li><a href="array.php">simple array and print outs</a></li>
-            <li><a href="ifstatement.php">if statements</a></li>
-            <li><a href="whiledowhileloop.php">while loop syntax</a></li>
-            <li><a href="switchstatement.php">switch statement syntax</a></li>
-            <li><a href="forloop.php">for loop syntax</a></li>
-            <li><a href="stringmanip.php">Simple String Manipulation</a></li>
-            <li><a href="datemanip.php">Date & Time Manipulation</a></li>
-            <li><a href="userdefinedfunc.php">User Defined Function</a></li>
+        <li><a href="array.php">simple array and print outs</a></li>
+        <li><a href="ifstatement.php">if statements</a></li>
+        <li><a href="whiledowhileloop.php">while loop syntax</a></li>
+        <li><a href="switchstatement.php">switch statement syntax</a></li>
+        <li><a href="forloop.php">for loop syntax</a></li>
+        <li><a href="stringmanip.php">Simple String Manipulation</a></li>
+        <li><a href="datemanip.php">Date & Time Manipulation</a></li>
+        <li><a href="userdefinedfunc.php">User Defined Function</a></li>
     </ul>
-</body>
-</html>
 
