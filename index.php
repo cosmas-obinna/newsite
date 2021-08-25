f329d98c328f043a821ec973e2fe0b0f1a0b4f0f
cosmas-obinna <obiynwajesus@gmail.com>
2021-08-25 16:27:22 +0100

    modification

--- index.php
+++ index.php
@@ -1,28 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Start</title>
-</head>
-<body>
+<?php include 'include/link.php'?>
     <h1> Hello World </h1><br/>
     <h3> This is my first php training web development.</h3><br/>
     <h3> I really hope to enjoy it. I was motivated to take this course</h3><br/>
     <h3> Because of the series of courses I have taken on sql</h3><br/>
-    <h2>follow each link to the respective topics</h2>
-    <ul>
-        <li><a href="array.php">simple array and print outs</a></li>
-        <li><a href="ifstatement.php">if statements</a></li>
-        <li><a href="whiledowhileloop.php">while loop syntax</a></li>
-        <li><a href="switchstatement.php">switch statement syntax</a></li>
-        <li><a href="forloop.php">for loop syntax</a></li>
-        <li><a href="stringmanip.php">Simple String Manipulation</a></li>
-        <li><a href="datemanip.php">Date & Time Manipulation</a></li>
-        <li><a href="userdefinedfunc.php">User Defined Function</a></li>
-    </ul>
-
 <?php
     $name = 'Cosmas Nworie';
     $Designation = 'Business Data Analyst';
