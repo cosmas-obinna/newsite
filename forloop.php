aa4ca620959a2f9879d46b9daf9ac178c0815653
cosmas-obinna <obiynwajesus@gmail.com>
2021-08-25 16:26:51 +0100

    modification

--- forloop.php
+++ forloop.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>For Loop</title>
-</head>
-<body>
+<!<?php include 'include/link.php'?>
     <h1>FOR LOOP</h1>
     <!-- For loop is sometimes refered to as a COUNTER CONTROL LOOP;
     meaning it does something for a certain number of times -->
